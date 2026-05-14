8dabc304a647c74a834dd4c67410e355ec51a970
alouis7 cyber <alouis7@ivytech.edu>
2026-05-14 18:43:34 -0400

    Push forms

--- orders/forms.py
+++ orders/forms.py
@@ -2,21 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class CustomerRegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = [
-            'customer_name',
-            'email',
-            'garment_type',
-            'alteration_type',
-            'notes',
-            'image',
-            'deadline',
-            'image',
-        ]
 
 class CustomerRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -25,10 +10,4 @@ class CustomerRegistrationForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-        from django import forms
-from .models import Order
-
-class HomeImageUploadForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields = ["customer_name", "email", "garment_type", "image"]
+        
